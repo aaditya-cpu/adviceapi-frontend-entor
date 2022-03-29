3d836e7b698a719c1b8798d00de3299f0a540628
Uzumaki <aaditya.uzumaki@gmail.com>
2022-03-29 22:05:50 +0530

    commit 3 home

--- ndex.js
+++ ndex.js
@@ -3,33 +3,44 @@ var requestOptions = {
     redirect: 'follow'
 };
 
-function apiCall() {
-    console.log("Iam alive")
-    fetch("https://api.adviceslip.com/advice", requestOptions)
+function revive() {
+    let x = Math.floor((Math.random() * 223) + 1);
+    let requrl = "https://api.adviceslip.com/advice/" + x;
+    console.log("requrl : " + requrl);
+    apiCall(requrl);
+}
+
+function apiCall(requrl) {
+    console.log("ET go home-...")
+
+    fetch(requrl, requestOptions)
         .then(response => response.json())
         .then(result => {
+
             var daa = result;
             var iD = daa.slip.id;
             var adv = daa.slip.advice;
             console.log("id: " + iD + " " + "advice: " + adv);
-            aliveCheck(iD, adv)
+            console.log("I am the data")
+            let numb = document.getElementById("adviseNumber");
+
+            let adviseText = document.getElementById("adviseText");
+
+            numb.innerHTML = iD;
+            adviseText.innerHTML = adv;
+            console.log("The fuction is complete check the data");
+            // aliveCheck(iD, adv)
         })
         .catch(error => console.log('error', error));
 
     // aliveCheck();
 }
 
-function aliveCheck(number, advice) {
+// function aliveCheck(number, advice) {
 
-    console.log("I am the data")
+//     console.log("I am the data")
 
-    let numb = document.getElementById("adviseNumber");
 
-    let adviseText = document.getElementById("adviseText");
+//     console.log("The fuction is complete check the data");
 
-    numb.innerHTML = number;
-    adviseText.innerHTML = advice;
-
-    console.log("The fuction is complete check the data");
-
-}
+// }
